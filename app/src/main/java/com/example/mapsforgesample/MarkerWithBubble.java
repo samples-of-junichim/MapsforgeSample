62f3131b88b982184abc0a9bfe5ff9a7a47b58f9
mor <junichi@mori-soft.com>
2017-05-30 14:23:02 +0900

    吹き出し付きマーカーを全体で一つだけ表示するように修正

--- app/src/main/java/com/example/mapsforgesample/MarkerWithBubble.java
+++ app/src/main/java/com/example/mapsforgesample/MarkerWithBubble.java
@@ -2,6 +2,7 @@ package com.example.mapsforgesample;
 
 import android.graphics.Color;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
@@ -19,6 +20,8 @@ import org.mapsforge.map.layer.overlay.Marker;
  */
 
 public class MarkerWithBubble extends Marker {
+
+    private static final String TAG = MarkerWithBubble.class.getSimpleName();
 
     private static final int DEFAULT_TEXT_SIZE = 15;
 
@@ -66,24 +69,43 @@ public class MarkerWithBubble extends Marker {
 
     @Override
     public boolean onTap(LatLong geoPoint, Point viewPosition, Point tapPoint) {
+
+        Log.d(TAG, "LoaLong: " + geoPoint.getLatitude() + ", " + geoPoint.getLongitude());
+        Log.d(TAG, "viewPos: " + viewPosition.x + ", " + viewPosition.y);
+        Log.d(TAG, "tapPos : " + tapPoint.x + ", " + tapPoint.y);
+
         if (contains(viewPosition, tapPoint)) {
+            Log.d(TAG, "contains: " + true);
 
             if (! TextUtils.isEmpty(mText)) {
+                Log.d(TAG, "text is exist");
+
                 if (null == mBalloon) {
+                    Log.d(TAG, "balloon is null");
+
                     createBalloon();
 
                     mMapView.addView(mBalloon, new MapView.LayoutParams(MapView.LayoutParams.WRAP_CONTENT, MapView.LayoutParams.WRAP_CONTENT,
-                            geoPoint, MapView.LayoutParams.Alignment.BOTTOM_CENTER));
+                            MarkerWithBubble.this.getLatLong(), MapView.LayoutParams.Alignment.BOTTOM_CENTER));
                 } else {
+                    Log.d(TAG, "balloon visibility is: " + mBalloon.getVisibility());
+
                     if (View.VISIBLE == mBalloon.getVisibility()) {
                         mBalloon.setVisibility(View.INVISIBLE);
                     } else {
                         mBalloon.setVisibility(View.VISIBLE);
                     }
                 }
+//                return true;
             }
+            Log.d(TAG, "text is null");
+        } else {
+            Log.d(TAG, "contains: " + false);
 
-            return true;
+            if (null != mBalloon) {
+                mBalloon.setVisibility(View.INVISIBLE);
+//                return true;
+            }
         }
         return super.onTap(geoPoint, viewPosition, tapPoint);
     }
